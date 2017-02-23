47848ffa0fdbbdbe1956b71e7dbadfc96710b24d
maria <maria@Warren.home>
2017-02-23 13:01:43 +0100

    Initial commit Main class TestWalk created

---
+++ RandomWalk/src/randomwalk/TestWalk.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package randomwalk;
+
+/**
+ *
+ * @author maria
+ */
+public class TestWalk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
