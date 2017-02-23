9e3aaf81ddf946cdcced7dbc7d7f1cd58658cb86
maria <maria@Warren.home>
2017-02-23 14:35:55 +0100

    Created and tested two RandomWalk objects in Main

--- RandomWalk/src/view/TestWalk.java
+++ RandomWalk/src/view/TestWalk.java
@@ -5,29 +5,59 @@
 // ************************************************************
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import control.*;
 
 public class TestWalk {
 
     public static void main(String[] args) {
-        int maxSteps; // maximum number of steps in a walk
-        int maxCoord; // the maximum x and y coordinate
-        int x, y; // starting x and y coordinates for a walk 
-        
+       
+        int maxSteps = 0; // maximum number of steps in a walk
+        int maxCoord = 0; // the maximum x and y coordinate
+        int x = 0, y = 0; // starting x and y coordinates for a walk
+
             
         Scanner scan = new Scanner(System.in);
             
         System.out.println("\nRandom Walk Test Program");
         System.out.println();
+        System.out.println("-------\tStart\t--------");
         
-        System.out.print("Enter the boundary for the square: ");
-        maxCoord = scan.nextInt();
+          //1-5) Random Walks
+        System.out.println("Random Walks:");
+        boolean endTry = false;
+
+        while (endTry == false) {
+            
+            try {
+                
+                System.out.println("Enter the boundary length:");
+                maxCoord = scan.nextInt();
+
+                System.out.print("Enter the maximum number of steps: ");
+                maxSteps = scan.nextInt();
+
+                System.out.print("Enter the starting x and y coordinates with "
+                        + "a space between: ");
+
+                x = scan.nextInt();
+                y = scan.nextInt();
+                             
+                
+                endTry = true;
+            } catch (InputMismatchException ex) {
+                System.out.println("Input Mismatch: Please type an integer\n");
+                endTry = false;
+            }
+        }
         
-        System.out.print("Enter the maximum number of steps: ");
-        maxSteps = scan.nextInt();
         
-        System.out.print("Enter the starting x and y coordinates with " + "a space between: ");
-        x = scan.nextInt();
-        y = scan.nextInt();
+        RandomWalk walk1 = new RandomWalk(10,5);
+       System.out.println("\nWalk1 initial position:\n" + walk1.toString());
+     
+         RandomWalk walk2 = new RandomWalk(maxCoord, maxSteps, x, y);         
+        System.out.println("\nWalk2 initial position:\n" + walk2.toString());
+      
     }
 }
